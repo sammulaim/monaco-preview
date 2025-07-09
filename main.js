611494eda62076bb2d5096967b2aca97d91eca4b
Shlomo Sam Mulaim <81721747+sammulaim@users.noreply.github.com>
2025-07-09 22:45:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 import * as monaco from 'monaco-editor';
 import 'monaco-editor/min/vs/editor/editor.main.css';
 
+const defaultHTML = `<html>
+  <body>
+    <h1>Hello from Monaco!</h1>
+  </body>
+</html>`;
+
 const editor = monaco.editor.create(document.getElementById('editor'), {
-  value: '<html>\n  <body>\n    <h1>Hello from Monaco!</h1>\n  </body>\n</html>',
+  value: defaultHTML,
   language: 'html',
   theme: 'vs-dark'
 });
 
 const preview = document.getElementById('preview');
+
+// Initial render
+preview.srcdoc = defaultHTML;
+
+// Update preview on change
 editor.onDidChangeModelContent(() => {
   const html = editor.getValue();
   preview.srcdoc = html;
